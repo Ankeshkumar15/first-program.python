a44478f1d072eb3dd694945ca0ab7071d43dd1ec
Ankeshkumar15 <142573054+Ankeshkumar15@users.noreply.github.com>
2024-04-15 21:24:53 +0530

    Update input.py

--- first.python/input.py
+++ first.python/input.py
@@ -1,9 +1,10 @@
-print("hello")
-#user input
 
+#get 3 integer from the user
+num1=int(input("enter the first interger:"))
+num2=int(input("enter the second interger:"))
+num3=int(input("enter the third interger:"))
 
-name=input("name :")
-print(name)
-
-#sum of two number
+#find the max
+maximum=max(num1,num2,num3)
+print("the maximum is:",maximum)
 
